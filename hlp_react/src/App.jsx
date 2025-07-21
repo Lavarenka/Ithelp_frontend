0240c73a7ddd314c764b451e99eb76c5bc6d5522
Mazaxaka_sf <arty_h41@mail.ru>
2025-07-22 01:26:45 +0300

    create Vite

---
+++ hlp_react/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+
+function App() {
+  
+  return (
+    <>
+      <div>
+        <p>Hello World</p>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
